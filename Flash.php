b73a49672762045bc9db6e80652eddc611daf994
Maurits van der Schee <maurits@vdschee.nl>
2018-10-25 16:57:22 +0200

    Initial commit

---
+++ Flash.php
@@ -0,0 +1,25 @@
+<?php
+namespace MindaPHP;
+
+class Flash
+{
+	static $flashSessionKey = 'flash';
+	
+	public static function set($type,$message)
+	{
+		if (!isset($_SESSION[static::$flashSessionKey])) $_SESSION[static::$flashSessionKey] = array();
+		$_SESSION[static::$flashSessionKey][$type] = $message;
+	}
+	
+	public static function get()
+	{
+		if (isset($_SESSION[static::$flashSessionKey])) {
+			$flash = $_SESSION[static::$flashSessionKey];
+			unset($_SESSION[static::$flashSessionKey]);
+		} else {
+			$flash = array();
+		}
+		return $flash;	
+	}
+	
+}
