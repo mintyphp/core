d694bd2e0e8dfe5762839ef9fb5df0f71ecffe7d
Maurits van der Schee <maurits@vdschee.nl>
2019-01-19 08:43:04 +0100

    support blob

--- src/DB.php
+++ src/DB.php
@@ -165,7 +165,7 @@ class DB
 
     $result = array();
     while ($query->fetch()) {
-      $result[] = json_decode(json_encode($row),true);
+      $result[] = unserialize(serialize($row));
     }
 
     $query->close();
