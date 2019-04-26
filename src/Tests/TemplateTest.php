e3b1e7e3e83b0bc1f964c244f8825a6192c10d8e
Maurits van der Schee <maurits@vdschee.nl>
2019-04-26 21:55:11 +0200

    add html escape test

--- src/Tests/TemplateTest.php
+++ src/Tests/TemplateTest.php
@@ -8,6 +8,7 @@ class TemplateTest extends \PHPUnit\Framework\TestCase
     public function testRender()
     {
         $this->assertEquals("hello World", Template::render('hello {{name|capitalize}}', ['name' => 'world'], ['capitalize' => 'ucfirst']));
+        $this->assertEquals("<br>hello &lt;br&gt;world", Template::render('<br>hello {{name}}', ['name' => '<br>world']));
         $this->assertEquals("hello {{name|failure!!function `failure` not found}}", Template::render('hello {{name|failure}}', ['name' => 'world'], ['capitalize' => 'ucfirst']));
         $this->assertEquals("hello m is 3", Template::render('hello {{if:n.m|eq(3)}}m is 3{{endif}}', ['n' => ['m' => 3]], ['eq' => function ($a, $b) {return $a == $b;}]));
         $this->assertEquals("hello not n", Template::render('hello {{if:n}}n{{else}}not n{{endif}}', ['n' => false]));
