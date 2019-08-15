28e00e9c166dded9350b38829be91ec2563735ae
Maurits van der Schee <maurits@vdschee.nl>
2019-08-15 16:01:06 +0200

    Update Token.php

--- src/Token.php
+++ src/Token.php
@@ -49,11 +49,7 @@ class Token
         switch ($algorithm[0]) {
             case 'H':
                 $hash = hash_hmac($hmac, $data, $secret, true);
-                if (function_exists('hash_equals')) {
-                    $equals = hash_equals($signature, $hash);
-                } else {
-                    $equals = $signature == $hash;
-                }
+                $equals = hash_equals($hash, $signature);
                 if (!$equals) {
                     return false;
                 }
