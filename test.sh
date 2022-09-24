6119ec0838eb5f08fbed662f0ce0b4c910ffca39
Maurits van der Schee <maurits@vdschee.nl>
2022-09-24 09:19:54 +0200

    update

--- test.sh
+++ test.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-if [ ! -f phpunit.phar ]; then
-  wget -O phpunit.phar https://phar.phpunit.de/phpunit-8.phar
-fi
-php phpunit.phar --bootstrap src/Loader.php src/Tests
+vendor/bin/phpunit src/Tests
