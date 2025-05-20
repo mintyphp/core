8210c1806292b3fea2c5b4eeaa25c4d6a65d4aac
Maurits van der Schee <maurits@vdschee.nl>
2025-05-20 18:16:44 +0200

    update

--- src/Schemas/Auth.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `users` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `username` varchar(255) COLLATE utf8_bin NOT NULL,
-    `password` varchar(255) COLLATE utf8_bin NOT NULL,
-    `totp_secret` varchar(255) COLLATE utf8_bin NOT NULL,
-    `created` datetime NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
