eea6ac513efb35d7946f8384ecf95158bef50330
Maurits van der Schee <maurits@vdschee.nl>
2025-06-05 02:08:33 +0200

    update

--- src/Orm.php
+++ src/Orm.php
@@ -5,7 +5,11 @@ namespace MintyPHP;
 class Orm
 {
     /**
-     * @param array<string, ?string> $object
+     * Inserts a new record into the specified table.
+     * 
+     * @param string $tableName The name of the table to insert into.
+     * @param array<string, ?string> $object An associative array representing the record to insert, where keys are column names and values are the corresponding values.
+     * @return int The ID of the newly inserted record. If no fields are provided, returns 0.
      */
     public static function insert(string $tableName, array $object): int
     {
@@ -27,9 +31,15 @@ class Orm
     }
 
     /**
-     * @param array<string, ?string> $object
+     * Updates an existing record in the specified table by its ID.
+     * 
+     * @param string $tableName The name of the table to update.
+     * @param array<string, ?string> $object An associative array representing the record to insert, where keys are column names and values are the corresponding values.
+     * @param string|int $id The ID of the record to update.
+     * @param string $idField The name of the ID field (default is 'id').
+     * @return bool Returns true if the update was successful, false otherwise.
      */
-    public static function update(string $tableName, array $object, string|int $id, string $idField = 'id'): int
+    public static function update(string $tableName, array $object, string|int $id, string $idField = 'id'): bool
     {
         if (isset($object[$idField])) {
             unset($object[$idField]);
@@ -51,16 +61,34 @@ class Orm
         $qmarks = implode(',', $qmarks);
         $sql = "UPDATE `$tableName` SET `$fields` = ? WHERE `$idField` = ?";
         $params[] = $id;
-        var_dump([$sql, $params]);
-        return DB::update($sql, $params);
+        return DB::update($sql, $params) ? true : false;
     }
 
     /**
-     * @return array<string, ?string> $object
+     * Selects a record from the specified table by its ID.
+     * 
+     * @param string $tableName The name of the table to select from.
+     * @param string|int $id The ID of the record to select.
+     * @param string $idField The name of the ID field (default is 'id').
+     * @return array<string, ?string> $object An associative array representing the selected record, where keys are column names and values are the corresponding values. If no record is found, an empty array is returned.
      */
     public static function select(string $tableName, string|int $id, string $idField = 'id'): array
     {
         $sql = "SELECT * FROM `$tableName` WHERE `$idField` = ?";
-        return DB::selectOne($sql, [$id])[$tableName] ?? [];
+        return DB::selectOne($sql, $id)[$tableName] ?? [];
+    }
+
+    /**
+     * Deletes a record from the specified table by its ID.
+     *
+     * @param string $tableName The name of the table.
+     * @param string|int $id The ID of the record to delete.
+     * @param string $idField The name of the ID field (default is 'id').
+     * @return bool Returns true if the deletion was successful, false otherwise.
+     */
+    public static function delete(string $tableName, string|int $id, string $idField = 'id'): bool
+    {
+        $sql = "DELETE FROM `$tableName` WHERE `$idField` = ?";
+        return DB::delete($sql, $id) ? true : false;
     }
 }
